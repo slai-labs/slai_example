65a791f4f46ef1daa73d345af609916c74307c00
Mike Risse <rissem@gmail.com>
2022-02-03 20:00:38 -0800

    add React to project

---
+++ static/calculator.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const Calculator = (props) => {
+  return <h1>Calculator</h1>
+}
+
+const domContainer = document.querySelector('#calculator')
+ReactDOM.render(React.createElement(Calculator), domContainer)
