df97119ca1ea8ea15b7cf314c882f3d781e6bbdd
Mike Risse <rissem@gmail.com>
2022-02-03 22:17:10 -0800

    working flow from JS->proxy->Slai and back

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask
 import slai
 import pandas
 import os
+import json
+from flask import request
 
 app = Flask(__name__)
 
@@ -10,17 +12,32 @@ slai.login(
     client_secret=os.environ["SLAI_CLIENT_SECRET"]
 )
 
-model = slai.model("rissem@gmail.com/california-housing-with-scikit-learn/linear-regression")
-
-data = {"latitude": [40.7, 22.7], "longitude": [-74.0, 23.0], "housing_median_age": [1,2], 
-        "total_rooms": [4,5], "total_bedrooms": [3,4], "population": [1,2], "households": [1,2], "median_income": [1,2]}
+model = slai.model("rissem@gmail.com/getting-started-ca-housing/initial")
 
 @app.route("/")
 def root():
     return app.send_static_file('index.html')
 
-@app.route("/slai")
+@app.route("/favicon.ico")
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+@app.route("/slai", methods = ["POST"])
 def slai_proxy():
+    data = {key: [request.json[key]] for key in request.json}
     df = pandas.DataFrame(data=data)
-    predictions = model(x1=df)    
-    return f"<p>Hello, World!<br />{predictions}</p>"
+    predictions = model(x1=df)["pred"]
+    return json.dumps(predictions.tolist())
+
+# For developer sanity. So changes to calculator.js do not require a hard refresh
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
--- static/calculator.js
+++ static/calculator.js
@@ -1,7 +1,50 @@
 'use strict'
 
+const { Box, Button, Container, CssBaseline } = MaterialUI
+
+// instead should take average people per household
+
+const initialInput = {
+  latitude: 32.7157,
+  longitude: -117.1611,
+  housing_median_age: 46,
+  total_rooms: 6000,
+  total_bedrooms: 1000,
+  population: 5000,
+  households: 3000,
+  median_income: 15,
+}
+
 const Calculator = (props) => {
-  return <h1>Calculator</h1>
+  const [input, setInput] = React.useState(initialInput)
+  const [result, setResult] = React.useState(null)
+
+  const fetchEstimate = async () => {
+    const response = await fetch('/slai', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
+    })
+    const result = (await response.json())[0]
+    setResult(result)
+  }
+
+  return (
+    <Container>
+      <CssBaseline />
+      <h1>California Housing Price Estimator</h1>
+      <div
+        onClick={() => {
+          fetchEstimate()
+        }}
+      >
+        Estimate
+        <h1>{result}</h1>
+      </div>
+    </Container>
+  )
 }
 
 const domContainer = document.querySelector('#calculator')
