5787aad2db4cbd9df3a89ceed3f40aad78a7e51c
Mike Risse <rissem@gmail.com>
2022-02-08 12:06:20 -0800

    smooth out some bumps in the installation process

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #! /bin/bash
-set -x -e
+set -e
 
-export FLASK_APP=server
+#ensure the following environment variables are set. SLAI_PROJECT_URL, SLAI_CLIENT_ID, SLAI_CLIENT_SECRET
+if [ -z "$SLAI_PROJECT_URL" ]; then
+  echo "SLAI_PROJECT_URL is not set"
+  exit 1
+fi
+
+if [ -z "$SLAI_CLIENT_ID" ]; then
+  echo "SLAI_CLIENT_ID is not set"
+  exit 1
+fi
+
+if [ -z "$SLAI_CLIENT_SECRET" ]; then
+  echo "SLAI_CLIENT_SECRET is not set"
+  exit 1
+fi
+
+export FLASK_APP=app
 export FLASK_ENV=development
 
 flask run --port=4242
